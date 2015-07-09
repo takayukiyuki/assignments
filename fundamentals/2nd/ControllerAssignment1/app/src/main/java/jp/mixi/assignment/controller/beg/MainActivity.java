9f9c72e05065d0c8d4089692e6c9382c38f8ec9f
takayukiyuki <takayuki.ando0403@gmail.com>
2015-07-09 21:17:48 +0900

    コミット

--- fundamentals/2nd/ControllerAssignment1/app/src/main/java/jp/mixi/assignment/controller/beg/MainActivity.java
+++ fundamentals/2nd/ControllerAssignment1/app/src/main/java/jp/mixi/assignment/controller/beg/MainActivity.java
@@ -4,21 +4,34 @@ package jp.mixi.assignment.controller.beg;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 /**
  * TODO: 課題1
- * {@link Toast#makeText(Context, CharSequence, int)} または
+ * {@link Toast#makeText(Contex t, CharSequence, int)} または
  * {@link Toast#makeText(Context, int, int)} を利用して、各ライフサイクルメソッドがどのような順番で
  * 実行されているかを確認してください。
  * 確認したら、assignments/fundamentals/2nd/Report.md にその順番を記述してください。
  * @author keishin.yokomaku
  */
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"number1",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"number2",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"number3",Toast.LENGTH_SHORT).show();
     }
 
     @Override
